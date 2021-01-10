94c9a6f72e4c222ec3c587ac0a498bed7f12b231
Kosuke Takahashi <kosuke.takahashi@bitkey.jp>
2021-01-11 03:08:30 +0900

    add safearea view

--- App.tsx
+++ App.tsx
@@ -1,15 +1,18 @@
-import React, { FC } from "react";
-import { Provider } from "react-redux";
-import AppContainer from "./src/AppContainer";
-import { store } from "./src/redux/redux";
+import React, { FC } from 'react'
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context'
+import { Provider } from 'react-redux'
+import AppContainer from './src/AppContainer'
+import { store } from './src/redux/redux'
 
 // TODO app.tsxもsrc/ 配下に入れたいけどbuildが通らなくなるので一旦このままにする
 const App: FC = () => {
   return (
     <Provider store={store}>
-      <AppContainer />
+      <SafeAreaProvider>
+        <AppContainer />
+      </SafeAreaProvider>
     </Provider>
-  );
-};
+  )
+}
 
-export default App;
+export default App
