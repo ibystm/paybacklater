94790eb0d0e52b263e63c044ac7948d48a89edbd
Kosuke Takahashi <kosuke.takahashi@bitkey.jp>
2021-01-16 23:04:11 +0900

    コード保存時に未使用のimportを削除する設定を追加

--- src/components/icons/PayOffButton.tsx
+++ src/components/icons/PayOffButton.tsx
@@ -1,27 +1,5 @@
 import React from 'react'
-import Svg, {
-  Circle,
-  Ellipse,
-  G,
-  Text,
-  TSpan,
-  TextPath,
-  Path,
-  Polygon,
-  Polyline,
-  Line,
-  Rect,
-  Use,
-  Image,
-  Symbol,
-  Defs,
-  LinearGradient,
-  RadialGradient,
-  Stop,
-  ClipPath,
-  Pattern,
-  Mask
-} from 'react-native-svg'
+import Svg, { Defs, LinearGradient, Path, Stop } from 'react-native-svg'
 
 const PayOffButton = () => {
   return (
