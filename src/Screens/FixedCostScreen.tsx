aa742cf5a482ebe48860c40bbf75545a9f565ebe
Kosuke Takahashi <kosuke.takahashi@bitkey.jp>
2021-01-31 02:02:44 +0900

    add fixed cost screen detail. (not detailed logic.)

--- src/Screens/FixedCostScreen.tsx
+++ src/Screens/FixedCostScreen.tsx
@@ -1,57 +1,96 @@
 import React, { FC } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { StyleSheet, Text, View, ViewStyle } from 'react-native'
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler'
+import { Colors } from '../color'
+import { FixedCostSettings } from '../types/FixedCostSettingTypes'
+import { P } from './stacks/AppContainer'
 
-interface P {
+type P = {
   onTapCloseModal: () => void
+  setFixedCost: () => void
 }
 
+type FixedCostSettingDataType = {
+  id: string
+  name: string
+  type: FixedCostSettings
+}
+
+const settingData: FixedCostSettingDataType[] = [
+  {
+    id: '1',
+    name: '月初',
+    type: 'beginningOfMonth'
+  },
+  {
+    id: '2',
+    name: '1５日',
+    type: 'middleOfMonth'
+  },
+  {
+    id: '3',
+    name: '月末',
+    type: 'endOfMonth'
+  },
+  {
+    id: '4',
+    name: '固定費にしない',
+    type: 'none'
+  }
+]
+
 const FixedCostScreen: FC<P> = ({ onTapCloseModal }) => {
+  const renderItem = ({ item }: { item: FixedCostSettingDataType }) => {
+    const borderStyle: ViewStyle = item.id === '4' ? { borderBottomWidth: 0 } : { borderBottomWidth: 0.3 }
+
+    return (
+      <TouchableOpacity style={{ ...styles.listButton, ...borderStyle }}>
+        <Text style={styles.listBtnText}>{item.name}</Text>
+      </TouchableOpacity>
+    )
+  }
+
   return (
     <View style={styles.centeredView}>
-      <SafeAreaView>
-        <View style={styles.centeredView}>
-          <Text>固定費</Text>
-        </View>
-      </SafeAreaView>
+      <FlatList data={settingData} renderItem={renderItem} />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   centeredView: {
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: '#fff',
+    flex: 1,
+    width: '100%',
+    justifyContent: 'flex-start',
+    paddingTop: 16,
+    paddingLeft: 24
   },
-  modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
+  upperArea: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 24
+  },
+  fixedCostTitle: {
     alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5
-  },
-  openButton: {
-    backgroundColor: '#F194FF',
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2
-  },
-  textStyle: {
-    color: 'white',
+    color: Colors.Main,
+    fontSize: 16,
     fontWeight: 'bold',
-    textAlign: 'center'
+    paddingTop: 5,
+    marginRight: 24
   },
-  modalText: {
-    marginBottom: 15,
-    textAlign: 'center'
+  listButton: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    width: '100%',
+    height: 40,
+    borderBottomColor: Colors.Gray8,
+    borderBottomWidth: 0.3
+  },
+  listBtnText: {
+    color: Colors.Gray8,
+    fontSize: 16
   }
 })
 
