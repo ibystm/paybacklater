7fad7b6e7c9a1630c5e8f858885fbb6c9480d6a8
Kosuke Takahashi <rhcp.kdog@gmail.com>
2021-05-05 03:40:26 +0900

    modified component type.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native'
-import React, { FC } from 'react'
+import React, { VFC } from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { Provider } from 'react-redux'
-import { store } from './src/redux/redux'
+import store from './src/redux/index'
 import RootStacks from './src/Screens/stacks/RootStacks'
 
 // TODO app.tsxもsrc/ 配下に入れたいけどbuildが通らなくなるので一旦このままにする
-const App: FC = () => {
+const App: VFC = () => {
   return (
     <Provider store={store}>
       <SafeAreaProvider>
